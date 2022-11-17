89ff854bfddb5ed43a54db5eea02bd4d13f40cb8
ErosCorrado <ecorrado@udesa.edu.ar>
2022-11-17 16:56:23 -0300

    cambios Eros

--- JS/search_results.js
+++ JS/search_results.js
@@ -1,6 +1,21 @@
-let urlUser = 'https://api.themoviedb.org/3/search/company?api_key=cf7707fd77a1290e2423ba7e39e253a8&page=1'
 let qs = location.search
 let qsobj = new URLSearchParams(qs);
 let buscador = qsobj.get('buscador')
 let titulo = document.querySelector('.letter')
 titulo.innerHTML = `TITULOS RELACIONADOS CON TU BUSQUEDA "${buscador}"`
+let urlUser = `ttps://api.themoviedb.org/3/search/multi?api_key=cf7707fd77a1290e2423ba7e39e253a8&language=en-US&query=${buscador}&page=1&include_adult=false`
+fetch(urlUser)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data)
+    
+
+
+  
+    return data;
+})
+.catch(function(error) {
+    return error;
+});
